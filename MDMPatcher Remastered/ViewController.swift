9873cfa4a064bdeb486cd56783a3d981e28c3e14
Jan Fabel <j4nf4b3l@outlook.com>
2021-04-08 11:22:18 +0200

    Initial Commit

---
+++ MDMPatcher Remastered/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MDMPatcher Remastered
+//
+//  Created by Jan Fabel on 08.04.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
